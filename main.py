0d997cc3204a31f609fc73fb7868e96b59d465b5
aabirbhattacharya1 <118759440+aabirbhattacharya1@users.noreply.github.com>
2022-11-22 19:16:37 +0530

    added discord bot

---
+++ main.py
@@ -0,0 +1,94 @@
+import discord
+import os
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+token = os.getenv('MTA0NDI4MTgzODQxMzI5OTcyMg.G2efRe.-uaLAlmF8iId5nEusGi5PEJSVUcQ3gS45fNTsc')
+intents = discord.Intents.all()
+client = discord.Client( intents=intents)
+ 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+
+@bot.event
+async def on_ready():
+	# CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
+	guild_count = 0
+
+	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+	for guild in bot.guilds:
+		# PRINT THE SERVER'S ID AND NAME.
+		print(f"- {guild.id} (name: {guild.name})")
+
+		# INCREMENTS THE GUILD COUNTER.
+		guild_count = guild_count + 1
+
+	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+@bot.event
+async def on_message(message):
+	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	if message.content == "hello":
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("hey!!! buddy")
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+  
+    print(f'Message {user_message} by {username} on {channel}')
+  
+    if message.author == client.user:
+        return
+    if channel == "random":
+        if user_message.lower() == "hello" or user_message.lower() == "hi":
+            await message.channel.send(f'Hello {username}')
+            return
+        elif user_message.lower() == "bye":
+            await message.channel.send(f'Bye {username}')
+        elif user_message.lower() == "tell me a joke":
+            jokes = [" Can someone please shed more\
+            light on how my lamp got stolen?",
+                     "Why is she called llene? She\
+                     stands on equal legs.",
+                     "What do you call a gazelle in a \
+                     lions territory? Denzel."]
+            await message.channel.send(random.choice(jokes))
+
+@bot.command()
+async def square(ctx, arg): # The name of the function is the name of the command
+    print(arg) # this is the text that follows the command
+    await ctx.send(int(arg) ** 2) # ctx.send sends text in chat
+
+@bot.command()
+async def scrabblepoints(ctx, arg):
+    # Key for point values of each letter
+    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+    points = 0
+    # Sum the points for each letter
+    for c in arg:
+        points += score[c]
+    await ctx.send(points)
+
+client.run(token)
